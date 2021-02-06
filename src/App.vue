55fc8f394edacf848e9125289d50154280d9ff2e
Ghozy Muhammad Hanzhalah Muchjidi <gliozghozy@gmail.com>
2021-02-07 01:53:32 +0700

    Update Router Structure

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,25 @@
 <template>
   <div id="app">
-    <navbar />
     <router-view />
   </div>
 </template>
 
 <script>
-import navbar from "./components/Navbar.vue";
-
-export default {
-  components: {
-    navbar,
-  },
-};
+  export default {
+    components: {},
+  };
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
-}
+  body,
+  html {
+    height: 100%;
+    margin: 0;
+  }
+
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
 </style>
