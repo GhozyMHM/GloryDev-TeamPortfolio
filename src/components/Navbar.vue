148bc8262f9eacf415720e9882cdceae5d9a68f0
Ghozy Muhammad Hanzhalah Muchjidi <gliozghozy@gmail.com>
2021-02-07 16:43:51 +0700

    Update Navbar Style

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,5 +1,5 @@
 <template>
-    <nav class="navbar navbar-expand-lg fixed-top">
+    <nav id="navbar" class="navbar navbar-expand-lg fixed-top">
         <div class="container-fluid">
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
                 data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
@@ -7,18 +7,18 @@
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav mx-auto mt-3 mb-2 mb-lg-0">
+                <ul id="ul" class="navbar-nav mx-auto">
                     <li class="nav-item">
-                        <router-link to="/">Home</router-link>|
+                        <router-link to="/" class="menu">Home</router-link><span class="line">|</span>
                     </li>
                     <li class="nav-item">
-                        <router-link to="/ghozy">Ghozy</router-link>|
+                        <router-link to="/ghozy" class="menu">Ghozy</router-link><span class="line">|</span>
                     </li>
                     <li class="nav-item">
-                        <router-link to="/ghozy">Rafi</router-link>|
+                        <router-link to="/ghozy" class="menu">Rafi</router-link><span class="line">|</span>
                     </li>
                     <li class="nav-item">
-                        <router-link to="/ghozy">Shiddiq</router-link>|
+                        <router-link to="/ghozy" class="menu">Shiddiq</router-link>
                     </li>
                 </ul>
             </div>
@@ -29,11 +29,46 @@
 <script>
     export default {
         name: 'navbar',
+        mounted() {
+            var navbar = document.getElementById('navbar');
+            var ul = document.getElementById('ul');
+            let menu = document.getElementsByClassName('menu');
+            let line = document.getElementsByClassName('line');
+            // let active = document.getElementsByClassName('router-link-exact-active');
+
+            window.onscroll = function () {
+                if (window.pageYOffset > 700) {
+                    navbar.style.background = "#276678";
+                    for (let x = 0, leng = menu.length; x < leng; x++) {
+                        menu[x].style.color = "#D9E6EA"
+                    }
+                    for (let y = 0, leng = line.length; y < leng; y++) {
+                        line[y].style.color = "#D9E6EA"
+                    }
+                    ul.style.margin = "5px 0";
+                } else {
+                    navbar.style.background = "transparent";
+                    for (let x = 0, leng = menu.length; x < leng; x++) {
+                        menu[x].style.color = "#1f5766"
+                    }
+                    for (let y = 0, leng = line.length; y < leng; y++) {
+                        line[y].style.color = "#1f5766"
+                    }
+                    ul.style.margin = "20px 0";
+                }
+            }
+        }
     }
 </script>
 
-<style>
-    @import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700;800;900&display=swap');
+<style scoped>
+    #navbar {
+        background-color: transparent;
+    }
+
+    #ul {
+        margin: 20px 0;
+    }
 
     nav ul li a {
         font-family: 'Montserrat', sans-serif;
@@ -41,6 +76,7 @@
         font-weight: 500;
         text-decoration: none;
         margin: 0 10px;
+        transition: ease-in-out;
     }
 
     nav ul li a.router-link-exact-active,
