d0794f13d13baf62cf1f923d612bb5e842abd6aa
Ghozy Muhammad Hanzhalah Muchjidi <gliozghozy@gmail.com>
2021-02-07 17:32:12 +0700

    Import Font Awesome Library

--- src/main.js
+++ src/main.js
@@ -1,6 +1,19 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import {
+  library
+} from '@fortawesome/fontawesome-svg-core'
+import {
+  faUserSecret
+} from '@fortawesome/free-solid-svg-icons'
+import {
+  FontAwesomeIcon
+} from '@fortawesome/vue-fontawesome'
+
+library.add(faUserSecret)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 Vue.config.productionTip = false
 
