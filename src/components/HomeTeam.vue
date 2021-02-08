86cbceab7eee4b2c33684709e4ca890e64cf429a
Ghozy Muhammad Hanzhalah Muchjidi <gliozghozy@gmail.com>
2021-02-09 03:36:13 +0700

    Add The Team Section in Home

---
+++ src/components/HomeTeam.vue
@@ -0,0 +1,171 @@
+<template>
+    <div class="home-team">
+        <div class="p-container-3">
+            <div class="p-content pc3">
+                <div data-aos="fade-down" class="theteam">
+                    <h1>The Team</h1>
+                    <p><i class="fas fa-caret-down"></i></p>
+                </div>
+                <div class="top-tri"></div>
+            </div>
+        </div>
+        <div class="p-container-4">
+            <div class="sp-cont">
+                <div data-aos="fade-left" class="row row-sp-cont">
+                    <div class="col-md-5 spc1">
+                        <img class="selfpict" src="../assets/img/ghozy.gif" alt="">
+                    </div>
+                    <div class="col-md-6 spc2">
+                        <h1>Ghozy Muhammad Hanzhalah Muchjidi</h1>
+                        <div class="row-bio">
+                            <div class="bio bio-tech">
+                                <h3>Web Developer</h3>
+                                <p>Common Used Tech :</p>
+                                <img class="tech" src="../assets/img/Vue Js.png" alt="">
+                                <img class="tech" src="../assets/img/JS.png" alt="">
+                                <img class="tech" src="../assets/img/Git.png" alt="">
+                            </div>
+                            <div class="bio bio-contact">
+                                <div class="contact-icon">
+                                    <a href="">
+                                        <p class="contact"><i class="fab fa-github"></i></p>
+                                    </a>
+                                    <a href="">
+                                        <p class="contact"><i class="fab fa-linkedin"></i></p>
+                                    </a>
+                                    <a href="">
+                                        <p class="contact"><i class="fab fa-instagram"></i></p>
+                                    </a>
+                                    <a href="">
+                                        <p class="contact"><i class="fab fa-whatsapp"></i></p>
+                                    </a>
+                                </div>
+                                <a class="contact" href="">See Works</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="p-container-4">
+            <div class="sp-cont">
+                <div data-aos="fade-right" class="row row-sp-cont">
+                    <div class="col-md-6 spc2">
+                        <h1>Muhamad Rafi Rahmat Alghifari</h1>
+                        <div class="row-bio">
+                            <div class="bio bio-tech">
+                                <h3>Mobile Developer</h3>
+                                <p>Common Used Tech :</p>
+                                <img class="tech" src="../assets/img/Vue Js.png" alt="">
+                                <img class="tech" src="../assets/img/JS.png" alt="">
+                                <img class="tech" src="../assets/img/Git.png" alt="">
+                            </div>
+                            <div class="bio bio-contact">
+                                <div class="contact-icon">
+                                    <a href="">
+                                        <p class="contact"><i class="fab fa-github"></i></p>
+                                    </a>
+                                    <a href="">
+                                        <p class="contact"><i class="fab fa-linkedin"></i></p>
+                                    </a>
+                                    <a href="">
+                                        <p class="contact"><i class="fab fa-instagram"></i></p>
+                                    </a>
+                                    <a href="">
+                                        <p class="contact"><i class="fab fa-whatsapp"></i></p>
+                                    </a>
+                                </div>
+                                <a class="contact" href="">See Works</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-5 spc1 spc1b">
+                        <img class="selfpict" src="../assets/img/ghozy.gif" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="p-container-4">
+            <div class="sp-cont">
+                <div data-aos="fade-left" class="row row-sp-cont">
+                    <div class="col-md-5 spc1">
+                        <img class="selfpict" src="../assets/img/ghozy.gif" alt="">
+                    </div>
+                    <div class="col-md-6 spc2">
+                        <h1>Muhammad Shiddiq Fathullah</h1>
+                        <div class="row-bio">
+                            <div class="bio bio-tech">
+                                <h3>UI/UX Designer</h3>
+                                <p>Common Used Tech :</p>
+                                <img class="tech" src="../assets/img/Vue Js.png" alt="">
+                                <img class="tech" src="../assets/img/JS.png" alt="">
+                                <img class="tech" src="../assets/img/Git.png" alt="">
+                            </div>
+                            <div class="bio bio-contact">
+                                <div class="contact-icon">
+                                    <a href="">
+                                        <p class="contact"><i class="fab fa-github"></i></p>
+                                    </a>
+                                    <a href="">
+                                        <p class="contact"><i class="fab fa-linkedin"></i></p>
+                                    </a>
+                                    <a href="">
+                                        <p class="contact"><i class="fab fa-instagram"></i></p>
+                                    </a>
+                                    <a href="">
+                                        <p class="contact"><i class="fab fa-whatsapp"></i></p>
+                                    </a>
+                                </div>
+                                <a class="contact" href="">See Works</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</template>
+
+<script>
+    export default {
+        name: "HomeTeam",
+        components: {},
+    };
+</script>
+
+<style scoped>
+    @import "../assets/home-style.css";
+    @import "../assets/home-team.css";
+
+
+
+    /* .shape {
+        width: 100%;
+        height: 80%;
+        margin-top: 85px;
+        background: #D9E6EA;
+        -moz-transform: scale(1.4) rotate(-10deg);
+        -webkit-transform: scale(1.4) rotate(-10deg);
+        -o-transform: scale(1.4) rotate(-10deg);
+        -ms-transform: scale(1.4) rotate(-10deg);
+        transform: scale(1.4) rotate(-10deg);
+    } */
+
+    /* .teamH {
+        width: 100%;
+        height: 100%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+    }
+
+    .teamH img {
+        width: 250px;
+    } */
+</style>
