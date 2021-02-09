78b43074252635a5f1b847cdea3168c8137294ea
Ghozy Muhammad Hanzhalah Muchjidi <gliozghozy@gmail.com>
2021-02-09 15:54:49 +0700

    Add Footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,30 @@
+<template>
+    <footer>
+        <div class="main">
+            <img src="../assets/img/Logo Footer.png" alt="GloryDev Logo">
+            <div class="main-team">
+                <h1>The Team</h1>
+                <p>Ghozy Muhammad Hanzhalah Muchjidi</p>
+                <p>Muhamad Rafi Rahmat Alghifari</p>
+                <p>Muhammad Shiddiq Fathullah</p>
+            </div>
+            <div class="main-contact">
+                <h1>Contact Us</h1>
+                <p><i class="far fa-envelope fa-fw"></i>glorydev@gmail.com</p>
+                <p><i class="fab fa-whatsapp fa-fw"></i>0838-9593-6423</p>
+                <p><i class="fab fa-instagram fa-fw"></i>GloryDev.Team</p>
+            </div>
+        </div>
+        <p class="copyright">GLORYDEV &copy; 2021. Made by Passion During Global Pandemic</p>
+    </footer>
+</template>
+
+<script>
+    export default {
+        name: "FooterContent",
+    };
+</script>
+
+<style scoped>
+    @import "../assets/footer-style.css";
+</style>
