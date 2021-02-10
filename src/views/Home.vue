b878396fd39b4ae315a959323b22ff9d337072ff
Ghozy Muhammad Hanzhalah Muchjidi <gliozghozy@gmail.com>
2021-02-11 03:20:03 +0700

    Give Navbar Function And Delete AOS Init

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="home">
-
     <HeaderHome />
 
     <div class="p-container-1">
@@ -54,31 +53,55 @@
     <HomeTeam />
 
     <FooterContent />
-
   </div>
 </template>
 <script>
-  import HeaderHome from "@/components/Header.vue";
-  import HomeTeam from "@/components/HomeTeam.vue";
-  import FooterContent from "@/components/Footer.vue";
-  import AOS from "aos";
+import HeaderHome from "@/components/Header.vue";
+import HomeTeam from "@/components/HomeTeam.vue";
+import FooterContent from "@/components/Footer.vue";
 
-  export default {
-    name: "Home",
-    components: {
-      HeaderHome,
-      HomeTeam,
-      FooterContent
-    },
+export default {
+  name: "Home",
+  components: {
+    HeaderHome,
+    HomeTeam,
+    FooterContent,
+  },
 
-    mounted() {
-      AOS.init({
-        duration: 700,
-        mirror: true,
-      });
-    },
-  };
+  mounted() {
+    window.onscroll = function () {
+      var navbar = document.getElementById("navbar");
+      var ul = document.getElementById("ul");
+      var dropmenu = document.getElementById("dropdown-menu");
+      let menu = document.getElementsByClassName("menu");
+      let line = document.getElementsByClassName("line");
+      // let active = document.getElementsByClassName('router-link-exact-active');
+
+      if (window.pageYOffset > 700) {
+        navbar.style.background = "#276678";
+        dropmenu.style.background = "#276678";
+        for (let x = 0, leng = menu.length; x < leng; x++) {
+          menu[x].style.color = "#D9E6EA";
+        }
+        for (let y = 0, leng = line.length; y < leng; y++) {
+          line[y].style.color = "#D9E6EA";
+        }
+        ul.style.margin = "5px 0";
+      } else {
+        navbar.style.background = "transparent";
+        dropmenu.style.background = "rgb(140, 207, 252)";
+        for (let x = 0, leng = menu.length; x < leng; x++) {
+          menu[x].style.color = "#1f5766";
+        }
+        for (let y = 0, leng = line.length; y < leng; y++) {
+          line[y].style.color = "#1f5766";
+        }
+        ul.style.margin = "20px 0";
+      }
+    };
+  },
+};
 </script>
 <style scoped>
-  @import "../assets/home-style.css";
+@import "../assets/home-style.css";
 </style>
