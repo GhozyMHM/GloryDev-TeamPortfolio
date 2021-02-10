9f5c4576753275bf9b45c59cfb1c1a75b7d4027d
Ghozy Muhammad Hanzhalah Muchjidi <gliozghozy@gmail.com>
2021-02-11 03:19:08 +0700

    Give FA Margin

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,7 +1,7 @@
 <template>
     <footer>
         <div class="main">
-            <img src="../assets/img/Logo Footer.png" alt="GloryDev Logo">
+            <img src="../assets/img/Logo Footer.png" alt="GloryDev Logo" />
             <div class="main-team">
                 <h1>The Team</h1>
                 <p>Ghozy Muhammad Hanzhalah Muchjidi</p>
@@ -10,12 +10,14 @@
             </div>
             <div class="main-contact">
                 <h1>Contact Us</h1>
-                <p><i class="far fa-envelope fa-fw"></i>glorydev@gmail.com</p>
-                <p><i class="fab fa-whatsapp fa-fw"></i>0838-9593-6423</p>
-                <p><i class="fab fa-instagram fa-fw"></i>GloryDev.Team</p>
+                <p><i class="far fa-envelope fa-fw"></i>&nbsp;glorydev@gmail.com</p>
+                <p><i class="fab fa-whatsapp fa-fw"></i>&nbsp;0838-9593-6423</p>
+                <p><i class="fab fa-instagram fa-fw"></i>&nbsp;GloryDev.Team</p>
             </div>
         </div>
-        <p class="copyright">GLORYDEV &copy; 2021. Made by Passion During Global Pandemic</p>
+        <p class="copyright">
+            GLORYDEV &copy; 2021. Made by Passion During Global Pandemic
+        </p>
     </footer>
 </template>
 
