d929bd05250e1dbafc53af1d8bedf518614b8ada
Ghozy Muhammad Hanzhalah Muchjidi <gliozghozy@gmail.com>
2021-02-11 03:20:35 +0700

    Add AOS Init

--- src/App.vue
+++ src/App.vue
@@ -5,23 +5,31 @@
 </template>
 
 <script>
-  export default {
-    components: {},
-  };
+import AOS from "aos";
+
+export default {
+  components: {},
+  mounted() {
+    AOS.init({
+      duration: 700,
+      mirror: true,
+    });
+  },
+};
 </script>
 
 <style>
-  @import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700;800;900&display=swap');
+@import url("https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700;800;900&display=swap");
 
-  body,
-  html {
-    height: 100%;
-    margin: 0;
-  }
+body,
+html {
+  height: 100%;
+  margin: 0;
+}
 
-  * {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-  }
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
 </style>
