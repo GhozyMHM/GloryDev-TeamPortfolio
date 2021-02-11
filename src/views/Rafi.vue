816ecf1ddadeb5f1ed95e487ae64cb62a5b57128
Ghozy Muhammad Hanzhalah Muchjidi <gliozghozy@gmail.com>
2021-02-11 10:25:48 +0700

    Add Rafi and Shiddiq Page

---
+++ src/views/Rafi.vue
@@ -0,0 +1,249 @@
+<template>
+    <div class="rafi">
+        <navbar />
+
+        <div class="member-banner">
+            <div class="col-md-5 member-pict">
+                <img class="selfpict" src="../assets/img/ghozy white.gif" alt="Ghozy (Member)" />
+            </div>
+            <div class="col-md-6 member-review">
+                <h1>Muhamad <span>Rafi</span> Rahmat Alghifari</h1>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam
+                    exercitationem minima consectetur, deleniti dolor corrupti nisi
+                    facilis autem reiciendis! Cum culpa, sit delectus quisquam
+                    exercitationem nostrum neque aliquid deleniti totam!
+                </p>
+            </div>
+        </div>
+
+        <div class="work-title">
+            <div data-aos="fade-down" class="work-title-cont">
+                <h1>My Latest Works</h1>
+                <p class="animate__animated animate__fadeOutDown animate__infinite">
+                    <i class="fas fa-angle-double-down"></i>
+                </p>
+            </div>
+        </div>
+
+        <div class="port1">
+            <div class="port-cont">
+                <div class="main-port">
+                    <!-- <img src="../assets/img/Holydamn.png" alt=""> -->
+                    <div id="portCarousel1" class="carousel slide" data-bs-ride="carousel">
+                        <div class="carousel-inner">
+                            <div class="carousel-item active">
+                                <img src="../assets/img/Holydamn 2.png" class="d-block w-100" alt="..." />
+                            </div>
+                            <div class="carousel-item">
+                                <img src="../assets/img/Holydamn 3.png" class="d-block w-100" alt="..." />
+                            </div>
+                            <div class="carousel-item">
+                                <img src="../assets/img/Holydamn 4.png" class="d-block w-100" alt="..." />
+                            </div>
+                        </div>
+                        <a class="carousel-control-prev" href="#portCarousel1" role="button" data-bs-slide="prev">
+                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span class="visually-hidden">Previous</span>
+                        </a>
+                        <a class="carousel-control-next" href="#portCarousel1" role="button" data-bs-slide="next">
+                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span class="visually-hidden">Next</span>
+                        </a>
+                    </div>
+                </div>
+                <div class="desc-port">
+                    <h1>HOLYDAMN</h1>
+                    <p>
+                        Classy and responsive landing page based on Progressive Web Apps
+                        (PWA) for clothing brand, HOLYDAMN
+                    </p>
+                    <p class="p-tu">Major Tech Used :</p>
+                    <div class="tech-used">
+                        <img src="../assets/img/Vue Js.png" alt="" />
+                        <img src="../assets/img/JS.png" alt="" />
+                        <img src="../assets/img/Git.png" alt="" />
+                    </div>
+                    <a href="">Visit Project</a>
+                </div>
+            </div>
+        </div>
+
+        <div class="port2">
+            <div class="port-cont">
+                <div class="desc-port">
+                    <h1>CLOIS88</h1>
+                    <p>
+                        University Class of Information System Majors Responsive Website
+                        Page
+                    </p>
+                    <p class="p-tu">Major Tech Used :</p>
+                    <div class="tech-used">
+                        <img src="../assets/img/Vue Js.png" alt="" />
+                        <img src="../assets/img/JS.png" alt="" />
+                        <img src="../assets/img/Git.png" alt="" />
+                    </div>
+                    <a href="">Visit Project</a>
+                </div>
+                <div class="main-port">
+                    <!-- <img src="../assets/img/Holydamn.png" alt=""> -->
+                    <div id="portCarousel2" class="carousel slide" data-bs-ride="carousel">
+                        <div class="carousel-inner">
+                            <div class="carousel-item active">
+                                <img src="../assets/img/Clois.png" class="d-block w-100" alt="..." />
+                            </div>
+                            <div class="carousel-item">
+                                <img src="../assets/img/Clois 2.png" class="d-block w-100" alt="..." />
+                            </div>
+                            <div class="carousel-item">
+                                <img src="../assets/img/Clois 3.png" class="d-block w-100" alt="..." />
+                            </div>
+                        </div>
+                        <a class="carousel-control-prev" href="#portCarousel2" role="button" data-bs-slide="prev">
+                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span class="visually-hidden">Previous</span>
+                        </a>
+                        <a class="carousel-control-next" href="#portCarousel2" role="button" data-bs-slide="next">
+                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span class="visually-hidden">Next</span>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="port1">
+            <div class="port-cont">
+                <div class="main-port">
+                    <!-- <img src="../assets/img/Holydamn.png" alt=""> -->
+                    <div id="portCarousel3" class="carousel slide" data-bs-ride="carousel">
+                        <div class="carousel-inner">
+                            <div class="carousel-item active">
+                                <img src="../assets/img/Heyboom.png" class="d-block w-100" alt="..." />
+                            </div>
+                            <div class="carousel-item">
+                                <img src="../assets/img/Heyboom 2.png" class="d-block w-100" alt="..." />
+                            </div>
+                            <div class="carousel-item">
+                                <img src="../assets/img/Heyboom 3.png" class="d-block w-100" alt="..." />
+                            </div>
+                        </div>
+                        <a class="carousel-control-prev" href="#portCarousel3" role="button" data-bs-slide="prev">
+                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span class="visually-hidden">Previous</span>
+                        </a>
+                        <a class="carousel-control-next" href="#portCarousel3" role="button" data-bs-slide="next">
+                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span class="visually-hidden">Next</span>
+                        </a>
+                    </div>
+                </div>
+                <div class="desc-port">
+                    <h1>HEYBOOM</h1>
+                    <p>
+                        Responsive and Beautiful landing page for photobook brand, HEYBOOM
+                    </p>
+                    <p class="p-tu">Major Tech Used :</p>
+                    <div class="tech-used">
+                        <img src="../assets/img/Vue Js.png" alt="" />
+                        <img src="../assets/img/JS.png" alt="" />
+                        <img src="../assets/img/Git.png" alt="" />
+                    </div>
+                    <a href="">Visit Project</a>
+                </div>
+            </div>
+        </div>
+
+        <div class="port2">
+            <div class="port-cont">
+                <div class="desc-port">
+                    <h1>COLOR PERSONALITY</h1>
+                    <p>
+                        Know your personality by typing your favorite color in this website
+                    </p>
+                    <p class="p-tu">Major Tech Used :</p>
+                    <div class="tech-used">
+                        <img src="../assets/img/Vue Js.png" alt="" />
+                        <img src="../assets/img/JS.png" alt="" />
+                        <img src="../assets/img/Git.png" alt="" />
+                    </div>
+                    <a href="">Visit Project</a>
+                </div>
+                <div class="main-port">
+                    <div id="portCarousel4" class="carousel slide" data-bs-ride="carousel">
+                        <div class="carousel-inner">
+                            <div class="carousel-item active">
+                                <img src="../assets/img/ColPer.png" class="d-block w-100" alt="..." />
+                            </div>
+                            <div class="carousel-item">
+                                <img src="../assets/img/ColPer 2.png" class="d-block w-100" alt="..." />
+                            </div>
+                            <div class="carousel-item">
+                                <img src="../assets/img/ColPer 3.png" class="d-block w-100" alt="..." />
+                            </div>
+                        </div>
+                        <a class="carousel-control-prev" href="#portCarousel4" role="button" data-bs-slide="prev">
+                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span class="visually-hidden">Previous</span>
+                        </a>
+                        <a class="carousel-control-next" href="#portCarousel4" role="button" data-bs-slide="next">
+                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span class="visually-hidden">Next</span>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <FooterContent />
+    </div>
+</template>
+
+<script>
+    import navbar from "@/components/Navbar.vue";
+    import FooterContent from "@/components/Footer.vue";
+
+    export default {
+        name: "Rafi",
+        components: {
+            navbar,
+            FooterContent,
+        },
+        mounted() {
+            window.onscroll = function () {
+                var navbar = document.getElementById("navbar");
+                var ul = document.getElementById("ul");
+                var dropmenu = document.getElementById("dropdown-menu");
+                let menu = document.getElementsByClassName("menu");
+                let line = document.getElementsByClassName("line");
+                // let active = document.getElementsByClassName('router-link-exact-active');
+
+                if (window.pageYOffset > 100) {
+                    navbar.style.background = "#276678";
+                    dropmenu.style.background = "#276678";
+                    for (let x = 0, leng = menu.length; x < leng; x++) {
+                        menu[x].style.color = "#D9E6EA";
+                    }
+                    for (let y = 0, leng = line.length; y < leng; y++) {
+                        line[y].style.color = "#D9E6EA";
+                    }
+                    ul.style.margin = "5px 0";
+                } else {
+                    navbar.style.background = "transparent";
+                    dropmenu.style.background = "rgb(140, 207, 252";
+                    for (let x = 0, leng = menu.length; x < leng; x++) {
+                        menu[x].style.color = "#1f5766";
+                    }
+                    for (let y = 0, leng = line.length; y < leng; y++) {
+                        line[y].style.color = "#1f5766";
+                    }
+                    ul.style.margin = "20px 0";
+                }
+            };
+        },
+    };
+</script>
+
+<style scoped>
+    @import url("../assets/team-style.css");
+</style>
