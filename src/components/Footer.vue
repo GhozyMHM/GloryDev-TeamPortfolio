1464991d12aaac883258f9a09f6702f68952614f
Ghozy Muhammad Hanzhalah Muchjidi <gliozghozy@gmail.com>
2021-02-14 01:53:51 +0700

    Update Footer Responsive Style

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,23 +1,34 @@
 <template>
     <footer>
-        <div class="main">
-            <img src="../assets/img/Logo Footer.png" alt="GloryDev Logo" />
-            <div class="main-team">
-                <h1>The Team</h1>
-                <p>Ghozy Muhammad Hanzhalah Muchjidi</p>
-                <p>Muhamad Rafi Rahmat Alghifari</p>
-                <p>Muhammad Shiddiq Fathullah</p>
-            </div>
-            <div class="main-contact">
-                <h1>Contact Us</h1>
-                <p><i class="far fa-envelope fa-fw"></i>&nbsp;glorydev@gmail.com</p>
-                <p><i class="fab fa-whatsapp fa-fw"></i>&nbsp;0838-9593-6423</p>
-                <p><i class="fab fa-instagram fa-fw"></i>&nbsp;GloryDev.Team</p>
+        <div class="row">
+            <div class="col-lg-12 main">
+                <div class="row">
+                    <div class="col-sm-12 col-lg-3 col-12 main-col logo">
+                        <img src="../assets/img/Logo Footer.png" alt="GloryDev Logo" />
+                    </div>
+                    <div class="col-sm-12 col-lg-4 col-12 main-col main-team">
+                        <h1>The Team</h1>
+                        <p>Ghozy Muhammad Hanzhalah Muchjidi</p>
+                        <p>Muhamad Rafi Rahmat Alghifari</p>
+                        <p>Muhammad Shiddiq Fathullah</p>
+                    </div>
+                    <div class="col-sm-12 col-lg-4 col-12 main-col main-contact">
+                        <h1>Contact Us</h1>
+                        <p><i class="far fa-envelope fa-fw"></i>&nbsp;glorydev@gmail.com</p>
+                        <p><i class="fab fa-whatsapp fa-fw"></i>&nbsp;0838-9593-6423</p>
+                        <p><i class="fab fa-instagram fa-fw"></i>&nbsp;GloryDev.Team</p>
+                    </div>
+                </div>
             </div>
         </div>
-        <p class="copyright">
-            GLORYDEV &copy; 2021. Made by Passion During Global Pandemic
-        </p>
+
+        <div class="row row-cpr">
+            <div class="col-sm-12 cpr">
+                <p class="copyright">
+                    GLORYDEV &copy; 2021. Made by Passion During Global Pandemic
+                </p>
+            </div>
+        </div>
     </footer>
 </template>
 
